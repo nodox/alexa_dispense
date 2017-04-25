d76eded21a30261de8d22879fd36d874cb959861
nodox <snatera09@gmail.com>
2017-04-24 22:09:17 -0500

    add pie chart

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 
+var userRegimen = {
+  hours: {
+    within1: 0,
+    within2: 0,
+    within3: 0,
+  }
+};
+
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
@@ -10,20 +18,49 @@ router.get('/pill', function(req, res, next) {
 
   res.io.emit("updatePillsDispensed", { day: 3, count: 1});
 
-  res.io.emit("updateDrugAdherenceChart", { 
-    taken: {
-      idx: 0,
-      count: 1,
-    },
-    dispensed: {
-      idx: 1,
-      count: 1
-    }
-  });
+  // res.io.emit("updateDrugAdherenceChart", { 
+  //   taken: {
+  //     idx: 0,
+  //     count: 1,
+  //   },
+  //   dispensed: {
+  //     idx: 1,
+  //     count: 1
+  //   }
+  // });
+
+  console.log(req.query);
+
+  var diff = Math.abs(parseInt(req.query.takenAt) - parseInt(userRegimen.prescribedTimeOfDay));
+
+  if (diff <= 1) {
+
+      userRegimen.hours.within1 += 1    
+  }
+
+  if (diff <= 2) {
+      userRegimen.hours.within2 += 1    
+
+  } 
+
+  if (diff >= 3) {
+    userRegimen.hours.within3 += 1    
+ 
+  }
+
+  res.io.emit("updateDrugAdherenceChart", userRegimen);
 
   res.end();
 });
 
+router.get('/regimen', function(req, res, next) {
+
+  userRegimen.prescribedDrugName = req.query.prescribedDrugName;
+  userRegimen.prescribedFrequency = req.query.prescribedFrequency;
+  userRegimen.prescribedTimeOfDay = req.query.prescribedTimeOfDay;
+
+  res.status(200).send({ done: true });
+});
 
 
 module.exports = router;
--- src/app/dashboard/dashboard.controller.js
+++ src/app/dashboard/dashboard.controller.js
@@ -1,24 +1,31 @@
 export class DashboardController {
-  constructor ($timeout, webDevTec, toastr, socket, $scope) {
+  constructor ($timeout, webDevTec, toastr, socket, $scope, $http) {
     'ngInject';
 
     this.$scope = $scope;
+    this.$http = $http;
     this.awesomeThings = [];
     this.classAnimation = '';
     this.creationDate = 1487304318548;
     this.toastr = toastr;
     this.hello = 'hello word';
 
+
+    // Display on the front dashboard somewhere
+    this.regimenSchedule = {};
+
+
     // Pie Chart
-    this.pieLabels = ["Administered", "Taken"];
-    this.pieData = [3, 5];
+    this.pieLabels = ["Taken within 1 hours", "Taken with 2 hours", "Taken within 3 or more hours"];
+    this.pieData = [0, 0, 0];
     this.pieOptions = {
       tooltipSize: 100
     };
-    socket.socket.on('updateDrugAdherenceChart', (data) => {
-      this.pieData[data.dispensed.idx] += data.dispensed.count;
-      this.pieData[data.taken.idx] += data.taken.count;
-    });
+
+    // socket.socket.on('updateDrugAdherenceChart', (data) => {
+    //   this.pieData[data.dispensed.idx] += data.dispensed.count;
+    //   this.pieData[data.taken.idx] += data.taken.count;
+    // });
 
     // Bar chart
     this.barLabels = ['Mon', 'Tue', 'Wed', 'Thur', 'Friday', 'Sat', 'Sun'];
@@ -29,8 +36,25 @@ export class DashboardController {
       this.barData[data['day']] += data.count;
     });
 
+    socket.socket.on('updateDrugAdherenceChart', (data) => {
+      this.pieData[0] = data.hours.within1;
+      this.pieData[1] = data.hours.within2;
+      this.pieData[2] = data.hours.within3;
+    });
 
+  }
 
+  createNewRegimen() {
+
+    // console.log(this.regimenSchedule.prescribedTimeOfDay.getTime()); // milliseconds
+
+    this.$http.get('/users/regimen', {params: this.regimenSchedule})
+      .then((res) => {
+        console.log('hello');
+
+      }, (err) => {
+        console.log(err);
+      });
 
   }
 
